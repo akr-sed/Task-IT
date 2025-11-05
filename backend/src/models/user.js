3d1d47b6aecee8d68835e7bcc60d58ede94046fb
aaayme <185227147+generalBenmalek@users.noreply.github.com>
2025-11-06 00:21:43 +0100

    feat: Add password hashing to User model pre-save hook
    Introduced a pre-save hook in the User Mongoose schema to automatically hash the passwordHash field using bcrypt before saving, enhancing security by ensuring passwords are stored as hashes.

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { hash } from "bcrypt";
 
 const userSchema = new mongoose.Schema(
     {
@@ -34,3 +35,22 @@ const userSchema = new mongoose.Schema(
         timestamps: { createdAt: true, updatedAt: false },
     }
 );
+
+
+// Hash before saving
+userSchema.pre("save", async function (next) {
+    // Skip on creation
+    if (this.isNew) return next();
+
+    if (!this.isModified("passwordHash")) return next();
+
+    try {
+        const saltRounds = 10;
+        this.passwordHash = await hash(this.passwordHash, saltRounds);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+export default mongoose.model("User", userSchema);
