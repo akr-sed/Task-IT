3846bed149906fc1c83a8a176427630e997bb00b
akr-sed <abderrehmane.mohamed.akram.seddik@ensia.edu.dz>
2025-11-05 19:20:14 +0100

    feat: create the databse connection and server starting

---
+++ backend/src/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+
+const extractMongoUsername = (uri) => {
+  try {
+    const regex = /mongodb(?:\+srv)?:\/\/([^:]+):/;
+    const match = uri.match(regex);
+    return match ? match[1] : "Unknown User";
+  } catch (err) {
+    return "Unknown User";
+  }
+};
+
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+connectDB().then(() => {
+  const mongoUsername = extractMongoUsername(process.env.MONGODB_URI);
+  console.log(`Connected to MongoDB as user: ${mongoUsername}`);
+  app.listen(PORT, () => {
+    console.log(`server started at port ${PORT}`);
+  });
+});
+
+
