eb7f156aa29169f7c3c0976b17d2238152de1819
aaayme <185227147+generalBenmalek@users.noreply.github.com>
2025-11-06 01:32:08 +0100

    feat: Add Project model schema for MongoDB
    Introduces a new Mongoose schema for projects, including fields for ownership, creation, name, description, and members with roles. The schema enforces required fields, unique project names, and timestamps for creation.

---
+++ backend/src/models/project.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+/**{
+ // this means it is an object ID of some object in the database
+  type: mongoose.Schema.Types.ObjectId,
+
+ // this tells mongoDB that this object is from users collection (record on the users table)
+  ref: "users"
+}*/
+
+
+const projectSchema = new mongoose.Schema(
+    {
+        ownedBy: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
+        createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
+        name: { type: String, required: true, unique: true },
+        description: String,
+        members: [
+            {
+                id: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+                role: { type: String, enum: ['admin', 'member'], default: 'member' }
+            }
+        ],
+    },
+    {
+        strict: 'throw',
+        timestamps: { createdAt: true, updatedAt: false },
+    }
+);
+
+export default mongoose.model('Project', projectSchema);
