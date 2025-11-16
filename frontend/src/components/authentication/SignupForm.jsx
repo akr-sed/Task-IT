8802a48c1b3006f17898fcb16afe3e04e3c69d10
akr-sed <abderrehmane.mohamed.akram.seddik@ensia.edu.dz>
2025-11-16 16:06:26 +0100

    refactor: fix the imports in the authentication components

--- frontend/src/components/authentication/SignupForm.jsx
+++ frontend/src/components/authentication/SignupForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { authService } from '../../api';
+
 import taskit from '../../assets/icons/task-it.svg';
 
 const SignupForm = () => {
