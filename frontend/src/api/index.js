6962527aef8d38598088f003b796ec949bf2c49d
akr-sed <abderrehmane.mohamed.akram.seddik@ensia.edu.dz>
2025-11-16 16:05:38 +0100

    refactor: add index api for easy imports

---
+++ frontend/src/api/index.js
@@ -0,0 +1,12 @@
+/**
+ * API Services Entry Point
+ * 
+ * Central export file for all API services
+ * Makes it easy to import services throughout the application
+ */
+
+export { default as api } from './axiosInstance';
+export { default as authService } from './authService';
+// export { default as projectService } from './projectService';
+// export { default as taskService } from './taskService';
+
