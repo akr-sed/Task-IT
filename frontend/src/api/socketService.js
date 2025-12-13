f6aead58fc43a33796a54e0171f7184c92f39d99
akr-sed <abderrehmane.mohamed.akram.seddik@ensia.edu.dz>
2025-12-13 19:16:50 +0100

    add vite_scocket_url

--- frontend/src/api/socketService.js
+++ frontend/src/api/socketService.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_API_URL?.replace("/api", "");
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 let socket = null;
 
