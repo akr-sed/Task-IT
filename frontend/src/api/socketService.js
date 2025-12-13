0b66aa320b6e1048283f95aeacc12919dd6fedf7
akr-sed <abderrehmane.mohamed.akram.seddik@ensia.edu.dz>
2025-12-13 18:59:31 +0100

    updating the socket

--- frontend/src/api/socketService.js
+++ frontend/src/api/socketService.js
@@ -50,6 +50,7 @@ export function initializeSocket() {
   }
 
   socket = io(SOCKET_URL, {
+    withCredentials : true,
     autoConnect: false,
     reconnection: true,
     reconnectionAttempts: 5,
